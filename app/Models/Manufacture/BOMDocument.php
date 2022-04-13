4b3b13659ec3e56e439d86ec34bd0cf6c2e7f94a
ROVIR15 <ekoarieanto92@gmail.com>
2022-04-13 08:25:54 +0700

    Add Model DB of BOM Document

---
+++ app/Models/Manufacture/BOMDocument.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Models\Manufacture;
+
+use Illuminate\Database\Eloquent\Model;
+
+class BOMDocument extends Model
+{
+    //
+    protected $table = 'bom_component';
+    
+}
