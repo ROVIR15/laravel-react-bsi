5bd2dfe4b938191812350f2f785871740bf760dd
ROVIR15 <ekoarieanto92@gmail.com>
2022-10-10 11:36:44 +0700

    fix kernel

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -19,7 +19,7 @@ class Kernel extends HttpKernel
         \Illuminate\Foundation\Http\Middleware\ValidatePostSize::class,
         \App\Http\Middleware\TrimStrings::class,
         \Illuminate\Foundation\Http\Middleware\ConvertEmptyStringsToNull::class,
-	\Fruitcake\Cors\HandleCors::class
+	    \Fruitcake\Cors\HandleCors::class
     ];
 
     /**
@@ -39,7 +39,7 @@ class Kernel extends HttpKernel
         ],
 
         'api' => [
-	    \Fruitcake\Cors\HandleCors::class,
+	        \Fruitcake\Cors\HandleCors::class,
             'throttle:60,1',
             'bindings',
         ],
