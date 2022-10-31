c108fc5b4b7ba45939763ba0661060cded3c8b56
ROVIR15 <ekoarieanto92@gmail.com>
2022-10-31 09:50:11 +0700

    fix search on monitoring display

--- resources/js/pages/dashboard/Monitoring/FinishedGoods/display.js
+++ resources/js/pages/dashboard/Monitoring/FinishedGoods/display.js
@@ -61,7 +61,7 @@ function applySortFilter(array, comparator, query) {
     return a[1] - b[1];
   });
   if (query) {
-    return filter(array, (_b) => _b.po_number.toLowerCase().indexOf(query.toLowerCase()) !== -1);
+    return filter(array, (_b) => _b.sales_order?.po_number.toLowerCase().indexOf(query.toLowerCase()) !== -1);
   }
   return stabilizedThis.map((el) => el[0]);
 }
