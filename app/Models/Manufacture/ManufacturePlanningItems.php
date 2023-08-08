4903c46c0876bf18f2ca0f27376befa7297a843d
ROVIR15 <ekoarieanto92@gmail.com>
2023-08-08 13:16:26 +0700

    test_sum_based_on_mpi

--- app/Models/Manufacture/ManufacturePlanningItems.php
+++ app/Models/Manufacture/ManufacturePlanningItems.php
@@ -48,7 +48,6 @@ class ManufacturePlanningItems extends Model
     public function ckckck(){
         return $this->hasMany('App\Models\Monitoring\Sewing', 'sales_order_id', 'sales_order_id')
         ->select('sales_order_id', 'order_item_id', 'facility_id', 'date', DB::raw('sum(output) as total_output, avg(output) as average_output, min(date) as real_start_date, max(date) as real_end_date'))
-        ->with('order_item')
         ->groupBy('facility_id', 'sales_order_id');
         // ->where('facility_id', '=', $this->facility);
     }
