2c27df4ef4dc368f0d558631ac9669e47ebcfcfc
ROVIR15 <ekoarieanto92@gmail.com>
2023-09-14 14:05:56 +0700

    fix finished goods recording

--- app/Http/Controllers/OrderItemController.php
+++ app/Http/Controllers/OrderItemController.php
@@ -142,9 +142,9 @@ class OrderItemController extends Controller
           ];
         });
 
-      $filtered = $orderItem->where('current_stock', '>', 0);
+      // $filtered = $orderItem->where('current_stock', '>', 0);
       // return new OrderItemCollection($orderItem);
-
+      $filtered = $orderItem
     } catch (Exception $th) {
       return response()->json([
         'success' => false,
--- resources/js/pages/dashboard/Monitoring/FinishedGoods/add.js
+++ resources/js/pages/dashboard/Monitoring/FinishedGoods/add.js
@@ -172,8 +172,8 @@ function WorkCenter() {
           return prevItems.map((row, index) => {
             if (row.id === parseInt(itemToUpdateIndex)) {
               if (editRowData[editedColumnName].value > row.qty_loading) {
-                alert('You trying to do something wrong! please check your input');
-                return row;
+                // alert('You trying to do something wrong! please check your input');
+                return { ...row, [editedColumnName]: editRowData[editedColumnName].value };
               }
               return { ...row, [editedColumnName]: editRowData[editedColumnName].value };
             } else {
