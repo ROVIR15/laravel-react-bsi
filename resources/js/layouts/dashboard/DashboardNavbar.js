6b47a16e7c24575b4b9f89408c9914f44073e63e
ROVIR15 <ekoarieanto92@gmail.com>
2023-09-17 09:25:40 +0700

    add notification component

--- resources/js/layouts/dashboard/DashboardNavbar.js
+++ resources/js/layouts/dashboard/DashboardNavbar.js
@@ -56,6 +56,7 @@ export default function DashboardNavbar({ onOpenSidebar }) {
         <Box sx={{ flexGrow: 1 }} />
 
         <Stack direction="row" alignItems="center" spacing={{ xs: 0.5, sm: 1.5 }}>
+          <NotificationsPopover />
           <AccountPopover />
         </Stack>
       </ToolbarStyle>
