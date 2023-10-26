6243af18863274ba8d16f5265e8642c0e6552f4c
ROVIR15 <ekoarieanto92@gmail.com>
2023-10-26 12:44:51 +0700

    hotfix

--- resources/js/pages/dashboard/OrderProgress/display.js
+++ resources/js/pages/dashboard/OrderProgress/display.js
@@ -231,10 +231,10 @@ function Display({ placeHolder }) {
           additi = {
             ...additi,
             percentage_c: additi.total_order > 0 ? Math.round((additi.cutting / additi.total_order) * 100) : 0,
-            percentage_s: additi.total_order > 0 ? Math.round(additi.sewing / additi.total_order) * 100 : 0,
-            percentage_qc: additi.total_order > 0 ? Math.round(additi.qc / additi.total_order) * 100 : 0,
+            percentage_s: additi.total_order > 0 ? Math.round((additi.sewing / additi.total_order) * 100) : 0,
+            percentage_qc: additi.total_order > 0 ? Math.round((additi.qc / additi.total_order) * 100) : 0,
             percentage_fg:
-              additi.total_order > 0 ? Math.round(additi.finished_goods / additi.total_order) * 100 : 0
+              additi.total_order > 0 ? Math.round((additi.finished_goods / additi.total_order) * 100) : 0
           };
 
           setData(additi);
