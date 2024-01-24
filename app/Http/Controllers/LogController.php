0cf7add1070e85ffa9cc1e62c1f336062a6128c8
ROVIR15 <ekoarieanto92@gmail.com>
2024-01-25 05:56:46 +0700

    hotfix

--- app/Http/Controllers/LogController.php
+++ app/Http/Controllers/LogController.php
@@ -3,7 +3,10 @@
 namespace App\Http\Controllers;
 
 use App\LogTable;
+use Carbon\Carbon;
 use Illuminate\Http\Request;
+
+use DB;
 
 class LogController extends Controller
 {
@@ -11,20 +14,27 @@ class LogController extends Controller
     public function index(Request $request)
     {
         try {
-            $query = LogTable::with('user')->orderBy('created_at', 'desc')->get()->map(function ($item, $index) {
-                return [
-                    'id' => $index + 1,
-                    'level' => $item->level,
-                    'url' => $item->url,
-                    'method' => $item->method,
-                    'message' => $item->message,
-                    'context' => $item->context,
-                    'user_id' => $item->user_id,
-                    'user_name' => $item->user->name,
-                    'created' => $item->created_at
-                ];
-            });
-            
+
+            $startDate = Carbon::today();
+            $endDate = Carbon::today()->addDays(7);
+
+            $query = LogTable::with('user')
+                ->whereBetween(DB::raw('DATE(created_at)'), [$startDate, $endDate])
+                ->orderBy('created_at', 'desc')
+                ->get()
+                ->map(function ($item, $index) {
+                    return [
+                        'id' => $index + 1,
+                        'level' => $item->level,
+                        'url' => $item->url,
+                        'method' => $item->method,
+                        'message' => $item->message,
+                        'context' => $item->context,
+                        'user_id' => $item->user_id,
+                        'user_name' => $item->user->name,
+                        'created' => $item->created_at
+                    ];
+                });
         } catch (\Throwable $th) {
             //throw $th;
             return response()->json([
